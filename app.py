10fe5db4f4ad6deecc250aa1e63e86a0113b11c3
Nick Steinmann <nick.steinmann@gmx.de>
2026-04-07 14:06:16 +0200

    Set up Flask app and database initialization

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
+import os
 from flask import Flask
+from data_manager import DataManager
+from models import db, Movie
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "MoviWeb läuft"
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/movies.db')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-if __name__ == "__main__":
+db.init_app(app)
+
+data_manager = DataManager()
+
+
+@app.route('/')
+def home():
+    return "Welcome to MoviWeb App!"
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
